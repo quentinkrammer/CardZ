a7f27d5ef01bf561fd22120bf1efaa673d5e1d83
Bastian Opatz <bopatz@brockhaus-ag.de>
2025-01-31 10:30:21 +0100

    setup dev, test, typecheck scripts

---
+++ backend/src/test.test.ts
@@ -0,0 +1,9 @@
+import { beforeEach, describe, expect, test } from "vitest";
+
+describe("test", () => {
+  beforeEach(async () => {});
+
+  test("someFunc", async () => {
+    expect(!!1).toEqual(true);
+  });
+});
---
+++ backend/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig, loadEnv } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+  console.log("Command: ", command);
+  console.log("Mode: ", mode);
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+
+  return {
+    define: { __APP_ENV__: JSON.stringify(env["APP_ENV"]) },
+    build: { outDir: "dist", emptyOutDir: true },
+  };
+});
