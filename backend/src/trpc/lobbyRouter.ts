8771f103785dd43e672150d84dbfba6b7a484728
Bastian Opatz <bopatz@brockhaus-ag.de>
2025-02-13 09:58:20 +0100

    backend: lobbyRouter

---
+++ backend/src/trpc/lobbyRouter.ts
@@ -0,0 +1,37 @@
+import { on } from "node:events";
+import { z } from "zod";
+import { createLobby } from "../drizzle/query/createLobby.js";
+import { joinLobby } from "../drizzle/query/joinLobby.js";
+import { leaveLobby } from "../drizzle/query/leaveLobby.js";
+import { authedProcedure, ee, t } from "./trpc.js";
+
+const subscriptionUrl = (obj: { lobbyId: string; userId: string }) =>
+  `lobby/${obj.lobbyId}/user/${obj.userId}`;
+
+export const lobbyRouter = t.router({
+  createLobby: authedProcedure.subscription(async function* ({
+    ctx: { userId },
+  }) {
+    const lobbyId = await createLobby();
+    await joinLobby({ lobbyId, userId });
+
+    for await (const [data] of on(ee, subscriptionUrl({ lobbyId, userId }))) {
+      yield data;
+    }
+  }),
+  joinLobby: authedProcedure
+    .input(z.object({ lobbyId: z.string() }))
+    .subscription(async function* ({ ctx: { userId }, input: { lobbyId } }) {
+      await joinLobby({ lobbyId, userId });
+
+      for await (const [data] of on(ee, subscriptionUrl({ lobbyId, userId }))) {
+        yield data;
+      }
+    }),
+  leaveLobby: authedProcedure
+    .input(z.object({ lobbyId: z.string() }))
+    .mutation(async ({ input: { lobbyId }, ctx: { userId } }) => {
+      await leaveLobby({ lobbyId, userId });
+      ee.removeAllListeners(subscriptionUrl({ lobbyId, userId }));
+    }),
+});
