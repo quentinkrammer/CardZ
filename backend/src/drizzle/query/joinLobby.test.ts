6f8cc3725a899c15923e0646cca98e94dafe684d
Bastian Opatz <bopatz@brockhaus-ag.de>
2025-02-20 12:33:14 +0100

    joinLobby test

---
+++ backend/src/drizzle/query/joinLobby.test.ts
@@ -0,0 +1,18 @@
+import { nanoid } from "nanoid";
+import { describe, expect, test } from "vitest";
+import { initiateDb } from "../initiateDb.js";
+import { createLobby } from "./createLobby.js";
+import { createUser } from "./createUser.js";
+import { joinLobby } from "./joinLobby.js";
+
+const {} = await initiateDb();
+const lobbyId = await createLobby();
+const userId = await createUser({ id: nanoid() });
+
+describe("insertQuests", () => {
+  test("insertQuests", async () => {
+    const res = await joinLobby({ lobbyId, userId });
+    expect(res[0]?.lobbyId).toBe(lobbyId);
+    expect(res[0]?.userId).toBe(userId);
+  });
+});
