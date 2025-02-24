fe03011e1c0419d67a3c58f0705d71350bac1b33
quentinkrammer <bopatzable@gmail.com>
2025-02-24 19:02:27 +0100

    background gradient

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,12 @@ import { Outlet } from "react-router";
 
 export function App() {
   return (
-    <>
+    <div
+      className="h-screen w-screen bg-radial from-gray-900 via-blue-800 to-blue-950 to-90% text-white"
+      // style={{ backgroundImage: "radial-gradient(circle, red, yellow, green)" }}
+    >
       <div>Header</div>
       <Outlet />
-    </>
+    </div>
   );
 }
