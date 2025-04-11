5a0688d9b857940ef7581ef7ed1d81f9acdf7073
Bastian Opatz <bopatz@brockhaus-ag.de>
2025-04-11 12:14:36 +0200

    add vite node plugin

--- backend/vite.config.ts
+++ backend/vite.config.ts
@@ -1,3 +1,4 @@
+import { node } from "@liuli-util/vite-plugin-node";
 import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
@@ -7,7 +8,11 @@ export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), "VITE_");
 
   return {
+    plugins: [node()],
     define: { __APP_ENV__: JSON.stringify(env["APP_ENV"]) },
-    build: { outDir: "dist", emptyOutDir: true },
+    build: {
+      outDir: "dist",
+      emptyOutDir: true,
+    },
   };
 });
