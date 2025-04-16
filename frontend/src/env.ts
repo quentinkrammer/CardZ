35336b83e0de8b3134dce14a28b67aa8151609af
Bastian Opatz <bopatz@brockhaus-ag.de>
2025-04-16 12:47:32 +0200

    cursed

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -3,9 +3,8 @@ import { z } from "zod";
 const untyped = {
   mode: import.meta.env.MODE,
   backendUrl: import.meta.env["VITE_BACKEND_URL"],
-  frontendUrl: import.meta.env["VITE_FRONTEND_URL"],
 };
 
 export const env = z
-  .object({ mode: z.string(), backendUrl: z.string(), frontendUrl: z.string() })
+  .object({ mode: z.string(), backendUrl: z.string() })
   .parse(untyped);
